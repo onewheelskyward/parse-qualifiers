f7544c40697e2f87f7252a8c578ab7ebf33f94a3
Andrew Kreps <andrew.kreps@gmail.com>
2013-08-07 13:50:48 -0700

    The gem code.

---
+++ lib/parse-qualifiers.rb
@@ -0,0 +1,19 @@
+class String
+	def parse_qualifiers()
+		# This should probably move to someplace more common, but it's not used anywhere else.
+		valid_operators = %w(eq gt gte lt lte ne contains)
+
+		parsed_qualifiers = []
+			qualifiers = self.split /,/
+			qualifiers.each do |qualifier|
+				matchdata = /([^;]+);([^;]+);+([^;]+)/.match(qualifier)
+				if matchdata && valid_operators.include?(matchdata[2])
+					# Three legged qualification
+					parsed_qualifiers.push([matchdata[1], matchdata[2], matchdata[3]])
+				else
+					# todo: logging
+				end
+			end
+		parsed_qualifiers
+	end
+end
