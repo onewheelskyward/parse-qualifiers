99b37bd3e4ec617dca55b339f7259310d94340ad
Andrew Kreps <andrew.kreps@gmail.com>
2013-08-07 14:48:10 -0700

    Adding summary to the specification.

--- lib/parse-qualifiers.rb
+++ lib/parse-qualifiers.rb
@@ -10,8 +10,6 @@ class String
 				if matchdata && valid_operators.include?(matchdata[2])
 					# Three legged qualification
 					parsed_qualifiers.push([matchdata[1], matchdata[2], matchdata[3]])
-				else
-					# todo: logging
 				end
 			end
 		parsed_qualifiers
